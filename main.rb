f132e47c41bb0f97bb2fd05d093d00c33fd70021
Chris White <cwprogram@live.com>
2011-10-07 18:08:47 -0600

    Got class imports working.

--- main.rb
+++ main.rb
@@ -4,4 +4,14 @@ Dir.glob('lib/*.jar') { | jar |
   require File.expand_path("../#{jar}", __FILE__)
 }
 
+def twitter4j
+  Java::Twitter4j
+end
 
+java_import twitter4j.Status
+java_import twitter4j.StatusAdapter
+java_import twitter4j.StatusDeletionNotice
+java_import twitter4j.StatusListener
+java_import twitter4j.TwitterException
+java_import twitter4j.TwitterStream
+java_import twitter4j.TwitterStreamFactory
